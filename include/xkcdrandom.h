2dba38995e694bde3b3cdbc060a4f7dddcef544f
Martijn <mrtijn@riseup.net>
2018-01-07 19:27:54 +0100

    Fix copyright notice

--- include/xkcdrandom.h
+++ include/xkcdrandom.h
@@ -1,21 +1,21 @@
 /*
  * xkcdrandom.h
- * This file is part of xkcdrandom
+ * This file is part of libxkcdrandom
  *
  * Copyright (c) 2018 Martijn
  *
- * xkcdrandom is free software: you can redistribute it and/or modify
+ * libxkcdrandom is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * xkcdrandom is distributed in the hope that it will be useful,
+ * libxkcdrandom is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with xkcdrandom.  If not, see <http://www.gnu.org/licenses/>.
+ * along with libxkcdrandom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef XKCDRANDOM_h
